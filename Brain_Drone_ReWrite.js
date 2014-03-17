ff6f668b28a8ad2f9ebf2d20772aeb513fab1df7
Robert Talamantez <robert.talamantez2@gmail.com>
2014-03-17 16:12:50 -0700

    Brain Drone

---
+++ Brain_Drone_ReWrite.js
@@ -0,0 +1,14 @@
+var DroneControls = {
+	this.init = function(droneIp)
+}
+
+//break headsets into objects
+
+var Headset = {
+	this.serialPort = {};
+	this.benchmark = {};
+	this.connection = {};
+	this.attention = {};
+	this.meditation = {};
+}
+
